1765ccb775f1b6a3172f7168a7819e350e9cfcf7
bryanleyton11 <bryan.leyton@uao.edu.co>
2026-05-19 22:43:45 -0500

    scena 3 arreglo
    subi lo de la vida ,sonido

--- ISLAND/Assets/Game/scrips/UIManager.cs
+++ ISLAND/Assets/Game/scrips/UIManager.cs
@@ -1,9 +1,38 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager Instance { get; private set; }
+
+    [Header("Menu Principal")]
     public GameObject panelInstrucciones;
+
+    [Header("In-Game")]
+    [SerializeField] private TextMeshProUGUI textoVidas;
+    [SerializeField] private GameObject panelGameOver;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        Time.timeScale = 1f;
+
+        if (panelGameOver != null)
+            panelGameOver.SetActive(false);
+    }
+
+    private void Start()
+    {
+        if (LifeManager.Instance != null && textoVidas != null)
+            ActualizarVidas(LifeManager.Instance.Vidas);
+    }
 
     public void Jugar()
     {
@@ -12,11 +41,34 @@ public class UIManager : MonoBehaviour
 
     public void Instrucciones()
     {
-        panelInstrucciones.SetActive(true);
+        if (panelInstrucciones != null)
+            panelInstrucciones.SetActive(true);
     }
 
     public void Salir()
     {
         Application.Quit();
+    }
+
+    public void ActualizarVidas(int vidas)
+    {
+        if (textoVidas != null)
+            textoVidas.text = vidas.ToString();
+    }
+
+    public void MostrarGameOver()
+    {
+        if (panelGameOver != null)
+            panelGameOver.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+    }
+
+    public void Reintentar()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
